1d4910bbea6ade49738a8693f42ed537cf484281
j3bronlam3s <j.c.perkins@wustl.edu>
2021-06-10 20:13:54 -0400

    Whack-a-Mole + Site Updates
    Completed wam games, and made some update to site including transfer to a php file.

--- index.php
+++ index.php
@@ -1,11 +1,5 @@
 
 <head>
-<!-- <?php
-$titles = ["Jebron Perkins", "Jebron.com", "This is not a website"];
-// $selectedTitle = $titles[array_rand($titles, 1)];
-$selectedTitle = $titles[$_REQUEST["title_id"]];
-echo "<title>$selectedTitle</title>"
-?> -->
     <title>Jebron Perkins</title>
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Dosis:wght@200&family=Montserrat&family=Raleway&family=Noto+Sans+KR:wght@100&display=swap" rel="stylesheet">
@@ -15,136 +9,277 @@ echo "<title>$selectedTitle</title>"
 <!-- entire page -->
 <div>
 
-<div id = "intro">
-<!-- ledt side whitespace -->
-<div style = "display: flex; flex: 2; flex-direction: column;">
-
-</div>
-<!-- center section -->
-<div style = "display: flex; flex: 6; flex-direction: column;">
-    <!-- whitespace above welcome -->
-        <div style = "flex: 2.5; background-color: transparent; color: transparent">
+    <!-- Intro -->
+    <div id = "intro">
+        <!-- let side whitespace -->
+        <div style = "display: flex; flex: 2; flex-direction: column;">
 
         </div>
-        <!-- Name -->
-        <div id = "welcome">
-            <h1>Jebron Perkins</h1>
-            <div id = "welcomeHLine"></div>
-            <h1 style = "font-family: 'Noto Sans KR', sans-serif;">&#xC81C;&#xBE0C;&#xB780; &#xD37C;&#xD0A8;&#xC2A4;</h1>
-        </div>
-        <!-- Whitespace between name and buttons -->
-        <div style = "flex: .25">
+        <!-- center section -->
+        <div style = "display: flex; flex: 6; flex-direction: column;">
+            <!-- whitespace above welcome -->
+                <div style = "flex: 2.5; background-color: transparent; color: transparent">
 
-        </div>
-        <!-- Buttons -->
-        <div style = "flex: 1.25; align-content: flex-start;">
-            <div class="boxWrapper">
+                </div>
+                <!-- Name -->
+                <div id = "welcome">
+                    <h1>Jebron Perkins</h1>
+                    <div id = "welcomeHLine"></div>
+                    <h1 style = "font-family: 'Noto Sans KR', sans-serif;">&#xC81C;&#xBE0C;&#xB780; &#xD37C;&#xD0A8;&#xC2A4;</h1>
+                </div>
+                <!-- Whitespace between name and buttons -->
+                <div style = "flex: .25">
 
-                    <div class = "box">
-                    <a href="#aboutMe" style="text-decoration: none; display: block; height: 100%;">
-                        <p>about me</p>
-                    </a>
                 </div>
-                    <div class = "boxBreaker">
-                        <p> </p>
-                    </div>
-                <div class = "box">
-                    <a href="#skills" style="text-decoration: none; display: block; height: 100%;">
-                        <p>skills</p>
-                    </a>
-                </div>
-                    <div class = "boxBreaker">
-                        <p> </p>
-                    </div>
-                <div class = "box">
-                    <a href="#projects" style="text-decoration: none;  display: block; height: 100%;">
-                        <p>projects</p>
-                    </a>
-                </div>
-                    <div class = "boxBreaker">
-                        <p> </p>
-                    </div>
-                <div class = "box">
-                    <a href="#tbd" style="text-decoration: none;  display: block; height: 100%;">
-                        <p>tbd</p>
-                    </a>
-                </div>
-                    <div class = "boxBreaker">
-                        <p> </p>
-                    </div>
-                <div class = "box">
-                    <a href="#contactMe" style="text-decoration: none;  display: block; height: 100%;">
-                        <p>contact me</p>
-                    </a>
-                </div>
+                <!-- Buttons -->
+                <div style = "flex: 1.25; align-content: flex-start;">
+                    <div class="boxWrapper">
 
+                            <div class = "box">
+                            <a href="#aboutMe" style="text-decoration: none; display: block; height: 100%;">
+                                <p>about me</p>
+                            </a>
+                        </div>
+                            <div class = "boxBreaker">
+                                <p> </p>
+                            </div>
+                        <div class = "box">
+                            <a href="#skills" style="text-decoration: none; display: block; height: 100%;">
+                                <p>skills</p>
+                            </a>
+                        </div>
+                            <div class = "boxBreaker">
+                                <p> </p>
+                            </div>
+                        <div class = "box">
+                            <a href="#projects" style="text-decoration: none;  display: block; height: 100%;">
+                                <p>projects</p>
+                            </a>
+                        </div>
+                            <div class = "boxBreaker">
+                                <p> </p>
+                            </div>
+                        <div class = "box">
+                            <a href="#tbd" style="text-decoration: none;  display: block; height: 100%;">
+                                <p>tbd</p>
+                            </a>
+                        </div>
+                            <div class = "boxBreaker">
+                                <p> </p>
+                            </div>
+                        <div class = "box">
+                            <a href="#contactMe" style="text-decoration: none;  display: block; height: 100%;">
+                                <p>contact me</p>
+                            </a>
+                        </div>
+
+                    </div>
+                </div>
+                <!-- bottom page whitespace -->
+                <div style = "flex: 1.5; color: transparent">
+
+                </div>
+            </div>
+        <!-- right section -->
+        <div style = "flex: 2; display: flex; flex-direction: column;">
+                <div class = "iconWrapper">
+                        <div class = "iconBreaker">
+                            <p> </p>
+                        </div>
+                        <a href = "https://github.com/j3bronlam3s"><img alt="Github Icon" src="githubIcon.png" width="65px" height="65px"></a>
+                        <div class = "iconBreaker">
+                            <p> </p>
+                        </div>
+                        <a href = "https://www.linkedin.com/in/jebron-perkins-4b74041a6/"><img alt="LinkedIn Icon" src="linkedinIcon.png" width="65px" height="65px"></a>
+                        <div class = "iconBreaker">
+                            <p> </p>
+                        </div>
+                        <a href = "https://open.spotify.com/user/thadivinecomedy"><img alt="Spotify Icon" src="spotifyIcon.png" width="65px" height="65px"></a>
+                        <div class = "iconBreaker">
+                            <p> </p>
+                        </div>
+                        <a href = "https://instagram.com/j3bron_lam3s"><img alt="Instagram Icon" src="instagramIcon.png" width="65px" height="65px"></a>
+                </div>
             </div>
         </div>
-        <!-- bottom page whitespace -->
-        <div style = "flex: 1.5; color: transparent">
 
-        </div>
     </div>
-<!-- right section -->
-<div style = "flex: 2; display: flex; flex-direction: column;">
-        <div class = "iconWrapper">
-                <div class = "iconBreaker">
-                    <p> </p>
-                </div>
-                <a href = "https://github.com/j3bronlam3s"><img alt="Github Icon" src="githubIcon.png" width="50px" height="50px"></a>
-                <div class = "iconBreaker">
-                    <p> </p>
-                </div>
-                <a href = "https://www.linkedin.com/in/jebron-perkins-4b74041a6/"><img alt="LinkedIn Icon" src="linkedinIcon.png" width="50px" height="50px"></a>
-                <div class = "iconBreaker">
-                    <p> </p>
-                </div>
-                <a href = "https://open.spotify.com/user/thadivinecomedy"><img alt="Spotify Icon" src="spotifyIcon.png" width="50px" height="50px"></a>
-                <div class = "iconBreaker">
-                    <p> </p>
-                </div>
-                <a href = "https://instagram.com/j3bron_lam3s"><img alt="Instagram Icon" src="instagramIcon.png" width="50px" height="50px"></a>
-        </div>
-    </div>
-</div>
 
-</div>
+    <!-- About Me -->
+    <div id = "aboutMe">
 
-<!-- About Me -->
-<!-- entire Page -->
-<div id = "aboutMe">
-
-    <div style="flex: 7; display: flex; flex-direction: row;">
-        <!-- photo -->
-        <div style="flex: 4; text-align: center;">
-            <img style="border: solid 10px rgb(181, 69, 84); margin: 10px; border-radius: 5%;" alt="Picture of Jebron Perkins" src="IMG_5542.jpg"height = "300vh">
-            <!-- <div class = "imageWrapper">
-                <img src="IMG_5542.jpg" height="10%" width="100%">
-                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-            </div> -->
+        <div style="flex: 7; display: flex; flex-direction: row;">
+            <!-- photo -->
+            <div style="flex: 4; text-align: center;">
+                <img style="border: solid 10px rgb(181, 69, 84); margin: 10px; border-radius: 5%;" alt="Picture of Jebron Perkins" src="IMG_5542.jpg"height = "550vh">
+            </div>
+            <!-- bio -->
+            <div style="flex: 6;" id = "bio">
+                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>    
+            </div>
+        
         </div>
-        <!-- bio -->
-        <div style="flex: 6;">
-            <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-            <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-            <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>    
+        <!-- timeline -->
+        <div style="flex: 3; color: transparent; text-align: center;">
+            <a href = "Jebron Perkins - Resume.pdf"><img src="timeline6.svg" height="375vh"></a>
         </div>
-    
-    </div>
-    <!-- timeline -->
-    <div style="flex: 3; color: transparent; text-align: center;">
-            <img src="timeline6.svg" height="250vh">
-    </div>
-    
+        
     </div>
 
     <!-- skills -->
-    <div id="skills" style="display: flex; flex: 1; text-align: center;">
-        <div class="vline"></div>
-        <div class="hline"></div>
+    <div id="skills">
+        <div>
+            <h1>1</h1>
+        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+
+        </div>
+        <div>
+        <h1>2</h1>
+
+        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+
+        </div>
+        <div>
+        <h1>3</h1>
+
+        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+
+        </div>
+        <div>
+        <h1>4</h1>
+
+        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+
+        </div>
+
     </div>
 
 <!-- page end -->
 </div>
 
 </body>
+
+<style>
+    /*
+    Colors
+    Black - #1F271B
+    White - #FEFFFE
+    Reddish - #B54554
+    Pinkish - #C2A0AC 
+    */
+    html{
+        scroll-behavior: smooth;
+    }
+    body{
+        font-family: 'Raleway', sans-serif;
+        color: #1F271B;
+        margin: 0px;
+        
+    }
+
+    #intro{
+        background-image:url(personalBackground.jpg);
+        background-repeat: no-repeat;
+        background-size: cover;
+        height: 100vh;
+        width: 100%;
+        display: flex;
+        flex-direction: row;
+    }
+    #welcomeHLine{
+        border-bottom: 10px solid #1F271B;
+        border-width:medium;
+        width: 80%;
+        margin: 0% 10% -5% 10%;
+    }
+    #welcome{
+        font-family: 'Dosis', sans-serif;
+        font-size:xx-large;
+        text-align: center;
+        color: #1F271B;
+        height: fit-content;
+        width: inherit;
+        margin: auto;
+        flex: 3;
+
+    }
+    .boxWrapper{
+        height:inherit;
+        width: inherit;
+        display: flex;
+    }
+    .box{
+        height: inherit;
+        width: height;
+        flex: 1;
+        background-color: #1F271B;
+        border-radius: 5%;
+        color: #FEFFFE;
+        text-align: center;
+        font-size: x-large;
+        display: flex;
+        flex-direction: column;
+    }
+    .box:hover{
+        background-color: rgb(32, 28, 28);
+        color:#FEFFFE;
+        box-shadow: 10px 10px 10px 10px rgb(181, 69, 84);
+        /* , 0 5px 50px 0 purple; */
+        opacity: 1;
+        }
+    .boxBreaker{
+        flex:.25;
+        height:inherit;
+        background-color: transparent;
+    }
+    a:visited, link{
+        color: #FEFFFE;
+    }
+    .iconWrapper{
+        height:inherit;
+        width: inherit;
+        display: flex;
+        flex-direction: column;
+        align-content:center;
+        align-self: flex-end;
+    }
+    .iconBreaker{
+        width: 10px;
+        height: inherit;
+        background-color: transparent;
+    }
+    #aboutMe{
+        background-image: url(aboutMeBackground.png);
+        height: 100vh;
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+    }
+    .imageWrapper{
+        height:inherit;
+        width:inherit;
+    }
+    #bio{
+        font-size: x-large;
+    }
+    #skills{
+        height: 100vh;
+        width: 100%;
+        background-color: black;
+        display: grid;
+        grid-column-gap: 5%;
+        grid-row-gap: 5vh;
+        grid-template-columns: auto auto;
+        grid-template-rows: auto auto;
+
+        
+    }
+    #skills div{
+        text-align: center;
+        font-size: x-large;
+        background-color: #B54554;
+    }
+
+</style>
